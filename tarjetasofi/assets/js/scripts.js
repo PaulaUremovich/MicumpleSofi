a535f0fff4a91b6efc5a621032437940b0529e06
PaulaUremovich <105173543+PaulaUremovich@users.noreply.github.com>
2023-01-09 00:05:44 -0300

    Add files via upload

---
+++ tarjetasofi/assets/js/scripts.js
@@ -0,0 +1,91 @@
+
+
+// Confirmar asistencia parallax
+$('.parallax-canciones').parallax({
+  imageSrc: 'assets/img/canciones.jpg'
+
+});
+
+// Portada parallax
+$('.parallax-portada').parallax({
+  imageSrc: 'assets/img/portada.jpg'
+});
+
+// Gracias parallax
+$('.parallax-gracias').parallax({
+  imageSrc: 'assets/img/portada.jpg'
+});
+
+// ----------------------
+
+// Cuenta regresiva
+var fechaInicio = new Date().getTime();
+var fechaFin = new Date(fechaCuentaRegresiva).getTime();
+var diff = (fechaFin - fechaInicio) / 1000;
+
+
+
+  // Config countdown
+  $('.countdown').ClassyCountdown({
+
+    theme: "black",
+
+    labelsOptions: {
+      style: 'font-size: 17px;font-family: \'Montserrat\', sans-serif;'
+    },
+
+    style: {
+      // textResponsive: 0.6,
+      secondsElement: {
+        gauge: {
+          fgColor: "#F00"
+        }
+      },
+
+      days: {
+        gauge: {
+          thickness: 0.01,
+          fgColor: colorCirculosCuentaRegresiva
+        },
+        textCSS: 'color:' + colorTextoCuentaRegresiva + ';font-size: 30px;font-family: \'Montserrat\', sans-serif;'
+      },
+
+
+      hours: {
+        gauge: {
+          thickness: 0.01,
+          fgColor: colorCirculosCuentaRegresiva
+        },
+        textCSS: 'color:' + colorTextoCuentaRegresiva + ';font-size: 30px !important;font-family: \'Montserrat\', sans-serif;'
+      },
+
+
+      minutes: {
+        gauge: {
+          thickness: 0.01,
+          fgColor: colorCirculosCuentaRegresiva
+        },
+        textCSS: 'color:' + colorTextoCuentaRegresiva + ';font-size: 30px !important;font-family: \'Montserrat\', sans-serif;'
+      },
+
+
+      seconds: {
+        gauge: {
+          thickness: 0.01,
+          fgColor: colorCirculosCuentaRegresiva
+        },
+        textCSS: 'color:' + colorTextoCuentaRegresiva + ';font-size: 30px !important;font-family: \'Montserrat\', sans-serif;'
+      }
+
+
+    },
+
+    // Fecha finalizacion
+    end: $.now() + diff,
+
+    // Al finalizar
+    onEndCallback: function () {
+      // $('.cuenta-regresiva').hide();
+    }
+
+  });
